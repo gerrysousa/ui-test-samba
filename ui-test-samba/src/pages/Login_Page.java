b9fe13b33bd80d97716535849d6ae4d1a3ad9eec
Gerry Sousa <gerry.nett@gmail.com>
2019-02-14 22:13:01 -0200

    [UI Teste] Add Chrome driver, e teste login com sucesso

--- ui-test-samba/src/pages/Login_Page.java
+++ ui-test-samba/src/pages/Login_Page.java
@@ -1,17 +1,33 @@
 package pages;
-import org.openqa.selenium.WebDriver; //biblioteca do WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver; //biblioteca do WebDriver;
+import org.openqa.selenium.By;
+import core.Base_Page;
 
+public class Login_Page extends Base_Page {
+	
+	public void setEmail(String email) {
+		escrever(By.id("inputEmail"), email);
+	}
 
-public class Login_Page {
+	public void setSenha(String senha) {
+		escrever(By.id("inputPassword"), senha);
+	}
+	
+	public void entrar() {
+		clicar(By.id("login"));
+	}
 
-	public static void main(String[] args) {
-
-		WebDriver driver = new FirefoxDriver(); //Cria uma nova instancia;
-		driver.get("http://quatest.com.br"); //sintaxe para abrir FIREFOX (O único drive instalado no momento é do firefox);
-		String i = driver.getCurrentUrl(); //Cria String i que é igual a getCurrentUrl: pega a url aberta no firefox;
-		System.out.println(i); //Mostra resultado obtido pela String i;
-		driver.close(); //Fecha navegador;
-
-		}
 }
+
+/*public class Login_Page {
+
+public static void main(String[] args) {
+
+	WebDriver driver = new FirefoxDriver(); //Cria uma nova instancia;
+	driver.get("http://quatest.com.br"); //sintaxe para abrir FIREFOX (O único drive instalado no momento é do firefox);
+	String i = driver.getCurrentUrl(); //Cria String i que é igual a getCurrentUrl: pega a url aberta no firefox;
+	System.out.println(i); //Mostra resultado obtido pela String i;
+	driver.close(); //Fecha navegador;
+
+	}
+}
+*/
--- ui-test-samba/src/tests/Login_Tests.java
+++ ui-test-samba/src/tests/Login_Tests.java
@@ -1,5 +1,55 @@
 package tests;
 
-public class Login_Tests {
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import core.Base_Test;
+import core.DriverFactory;
+import pages.Login_Page;
+
+public class Login_Tests extends Base_Test {
+
+	private Login_Page login;
+	//private static WebDriver driver;
+	
+	@Before
+	public void inicializaTeste() {
+		//driver.get("http://www.google.com.br");
+		DriverFactory.getDriver().get("http://web1.qa.sambatech.com:10000");
+		 login = new Login_Page();
+		
+	}
+	
+	
+	@Test
+	public void deveLogarComSucesso(){
+		login.setEmail("avaliacao_qa_samba@sambatech.com.br");
+		login.setSenha("123456789");
+		login.entrar();
+		
+		
+	}
 }
+
+
+
+
+
+/*	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
+		
+		
+		/*WebDriver driver = new ChromeDriver(); //Cria uma nova instancia;
+		driver.get("http://quatest.com.br"); //sintaxe para abrir FIREFOX (O único drive instalado no momento é do firefox);
+		String i = driver.getCurrentUrl(); //Cria String i que é igual a getCurrentUrl: pega a url aberta no firefox;
+		System.out.println(i); //Mostra resultado obtido pela String i;
+		driver.close(); //Fecha navegador;
+*/
+		
+
